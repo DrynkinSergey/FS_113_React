71c245f7a1eca45443ee7b638f3db471a4070140
Drynkin Sergey <sergsmile29@gmail.com>
2025-04-24 22:04:55 +0300

    redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css';
 import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-    <Toaster />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      <Toaster />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,34 @@ import { configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from './counterSlice';
 import { todosReducer } from './todosSlice';
 import { filterReducer } from './filterSlice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 console.log(import.meta.env);
+
+const persistConfig = {
+  key: 'todos',
+  version: 1,
+  storage,
+};
+const persistConfigFilter = {
+  key: 'filter',
+  version: 1,
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    todolist: todosReducer,
-    filter: filterReducer,
+    todolist: persistReducer(persistConfig, todosReducer),
+    filter: persistReducer(persistConfigFilter, filterReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: import.meta.env.MODE === 'development',
 });
+export let persistor = persistStore(store);
